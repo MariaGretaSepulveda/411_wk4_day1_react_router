21d8048d95451ee4a2d84c78f7cc92bff384d080
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-10-02 20:48:20 -0500

    project react router updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navigation from './components/Navigation'
-import BrowserRouter from './react-router-dom'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import Router from './Router';
+
 // Write imports for Router & BrowserRouter here //
 
 function App() {
   return (
-    <Navigation />
+    <BrowserRouter>
+      <Navigation />
+      <Router />
+    </BrowserRouter>
   );
 }
 
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import cars from '../cars.json'
-// import material ui components here //
-// Container, Paper, Chip //
+import { Container, Paper, Chip, Card, CardContent } from '@material-ui/core';
 
 const Car = (props) => {
+    const id = props.match.params.id
+    const currentCar = cars.find(c => {
+        return c.id === id;
+    })
+
     return (
-        <h1>A specific car</h1>
+        <Card>
+            <CardContent>
+                <Container className="car-container">
+                    <Paper className="car-paper">
+                        {currentCar.Name}
+                        <br></br>
+                        {Object.keys(currentCar).map((key, idx) => {
+                            return <Chip label={`${key}: ${currentCar[key]}`} />
+                        })}
+
+                    </Paper>
+                </Container>
+            </CardContent>
+        </Card>
+
+
     )
 }
 
-export default Car
+
+
+export default Car;
